fd704a6b182845a693b25755b5bfc57da704f280
Yaroslav Melnychuk <melnichukjarik@gmail.com>
2018-08-30 22:04:53 +0300

    Init project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <h1>Code here</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
