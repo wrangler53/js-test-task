68ed03ef0da72ed4c6d8104359beeb9b5dc0b799
Yaroslav Melnychuk <melnichukjarik@gmail.com>
2018-09-01 13:18:18 +0300

    Added form validation (button is disabled, if one of the fields are empty)

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,63 +1,73 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Form = ({addUser, editUser, user, inputChanged, isEditMode}) => (
-  <div className="container">
-    <h2>Form</h2>
-    <form
-      className="form"
-      onSubmit={
-        !isEditMode ? event => addUser(event) : event => editUser(event)
-      }
-    >
-      <div className="form__item">
-        <label htmlFor="first_name">First Name</label>
-        <input
-          type="text"
-          name="first_name"
-          id="first_name"
-          value={user.first_name}
-          onChange={event => inputChanged(event)}
-        />
-      </div>
-      <div className="form__item">
-        <label htmlFor="last_name">Last name</label>
-        <input
-          type="text"
-          name="last_name"
-          id="last_name"
-          value={user.last_name}
-          onChange={event => inputChanged(event)}
-        />
-      </div>
-      <div className="form__item">
-        <label htmlFor="dob">Date of birth</label>
-        <input
-          type="text"
-          name="dob"
-          id="dob"
-          placeholder="mm/dd/yyyy"
-          value={user.dob}
-          onChange={event => inputChanged(event)}
-        />
-      </div>
-      <div className="form__item">
-        <label htmlFor="location">Location</label>
-        <input
-          type="text"
-          name="location"
-          id="location"
-          value={user.location}
-          onChange={event => inputChanged(event)}
-        />
-      </div>
-      <div className="form__item">
-        <input type="submit" value={!isEditMode ? 'Add user' : 'Edit User'} />
-      </div>
-    </form>
-  </div>
-);
+const Form = ({addUser, editUser, user, inputChanged, isEditMode}) => {
+  const isDisabled = Object.values(user)
+    .map(item => item.length)
+    .some(item => item === 0);
 
+  return (
+    <div className="container">
+      <h2>Form</h2>
+      <form
+        className="form"
+        onSubmit={
+          !isEditMode ? event => addUser(event) : event => editUser(event)
+        }
+      >
+        <div className="form__item">
+          <label htmlFor="first_name">First Name</label>
+          <input
+            type="text"
+            name="first_name"
+            id="first_name"
+            value={user.first_name}
+            onChange={event => inputChanged(event)}
+          />
+        </div>
+        <div className="form__item">
+          <label htmlFor="last_name">Last name</label>
+          <input
+            type="text"
+            name="last_name"
+            id="last_name"
+            value={user.last_name}
+            onChange={event => inputChanged(event)}
+          />
+        </div>
+        <div className="form__item">
+          <label htmlFor="dob">Date of birth</label>
+          <input
+            type="text"
+            name="dob"
+            id="dob"
+            placeholder="mm/dd/yyyy"
+            value={user.dob}
+            onChange={event => inputChanged(event)}
+          />
+        </div>
+        <div className="form__item">
+          <label htmlFor="location">Location</label>
+          <input
+            type="text"
+            name="location"
+            id="location"
+            value={user.location}
+            onChange={event => inputChanged(event)}
+          />
+        </div>
+        <div className="form__item">
+          <input
+            type="submit"
+            className="btn btn_submit"
+            disabled={isDisabled}
+            value={!isEditMode ? 'Add user' : 'Edit User'}
+          />
+        </div>
+      </form>
+    </div>
+  );
+};
 Form.propTypes = {
   addUser: PropTypes.func.isRequired,
   editUser: PropTypes.func.isRequired,
